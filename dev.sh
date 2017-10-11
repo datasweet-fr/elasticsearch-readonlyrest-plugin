d5e45fe4b760ffa400fa6f93f6805cd392b7a8fd
Thomas Charlot <thomas.charlot@datasweet.fr>
2017-10-11 15:49:13 +0200

    Add dev bash

---
+++ dev.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# ATTENTION ! Si vous changez le dossier penser à changer également dans ./dev/elasticsearch.yml
+ES_DIR=/Users/tcharlot/Downloads/elasticsearch-5.5.2
+source  ./dev/read_ini.sh
+read_ini ./gradle.properties
+
+./gradlew clean updateSHAs assemble install
+
+#pkill  -f elasticsearch
+$ES_DIR/bin/elasticsearch-plugin remove ${INI__pluginName}
+$ES_DIR/bin/elasticsearch-plugin install file://$PWD/build/distributions/${INI__pluginName}-${INI__pluginVersion}_es${INI__esVersion}.zip
+cp -f ./dev/elasticsearch.yml $ES_DIR/config/
+cp -f ./dev/elasticsearch-ror.yml $ES_DIR/config/
+$ES_DIR/bin/elasticsearch
